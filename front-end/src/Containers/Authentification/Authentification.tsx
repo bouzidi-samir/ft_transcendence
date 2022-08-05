040f6c0f339bc7cfd477ee87918e1708aaeea523
Bouzidi Samir <fouleck30@hotmail.fr>
2022-08-05 20:37:15 +0200

    authentification form in progress

--- front-end/src/Containers/Authentification/Authentification.tsx
+++ front-end/src/Containers/Authentification/Authentification.tsx
@@ -1,10 +1,25 @@
+import "./Authentification.css"
 import React from "react";
 
 export default function Authentification() {
   return (
     <div className="auth-content">
-      <h1>Authentification</h1>
-    
+        <div className="auth-field">
+          <form className="auth-form">
+          <h1>Pong Master</h1>
+              <input></input>
+              <input></input>
+              <button className="btn btn-primary"> Login </button>
+              <hr></hr>
+              <p>or</p>
+          </form>
+        </div>
+        
+        <div className="picture">
+        
+        </div>
+
+
     </div>
   )
 }
