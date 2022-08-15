5443adb7efd9f2fe399d579ddaecdd476cc813a5
Bouzidi Samir <fouleck30@hotmail.fr>
2022-08-15 18:53:27 +0200

    src/

--- front-end/src/Containers/Accueil/Accueil.tsx
+++ front-end/src/Containers/Accueil/Accueil.tsx
@@ -1,18 +1,33 @@
 import "./Accueil.css"
-import React from "react";
+import React, {useState} from "react";
+import Custom from "./Components/Custom/Custom";
 
 export default function Accueil() {
+
+    const [custom, setCustom] = useState(false);
+    const [newMenber, setNewMenber] = useState(true);
+
+    function redirection() {
+        if (newMenber == true)
+            setCustom(true);
+    }
 
     return (
         <>
         <div className="custom-content">
            <div className="pong" data-aos="fade-up" data-aos-duration="2000"></div>
-            <div className="custom-title">
-                <h1  data-aos="fade-right" data-aos-duration="2000">Master Pong</h1>
-                <button data-aos="fade-left"  data-aos-duration="2000" className="btn btn-primary">
-                    Commencer
-                </button>
-            </div>
+            {custom == false ?
+                
+                <div className="custom-title">
+                    <h1  data-aos="fade-right" data-aos-duration="2200">Master Pong</h1>
+                    <button  onClick={redirection}
+                        data-aos="fade-left"  data-aos-duration="2200" 
+                        className="btn btn-primary">
+                        Commencer
+                    </button>
+                </div>
+            
+            : <Custom/>}
             <div className="pong" data-aos="fade-down" data-aos-duration="2000"></div>
         </div>
         </>
---
+++ front-end/src/Containers/Accueil/Components/Custom/Custom.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Custom.css"
+
+export default function Custom() {
+  return (
+    <div className="custom-form" data-aos="fade-up" data-aos-duration="1000">
+        
+        <div className="vignette"></div>
+        <form >
+            <h2>Choisie ton pseudo</h2>
+            <input></input>
+            <button className="btn btn-primary">Valider</button>
+        </form>
+    </div>
+  )
+}
