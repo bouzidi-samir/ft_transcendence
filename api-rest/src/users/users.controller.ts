821ef98c5ffbccb36a7c949d3cd6fe9ff48f68ac
bouzidi-samir <fouleck30@hotmail.fr>
2022-09-01 16:07:03 +0200

    use context init

--- api-rest/src/users/users.controller.ts
+++ api-rest/src/users/users.controller.ts
@@ -1,15 +1,30 @@
 import { Controller, Get, Inject, Param } from '@nestjs/common';
-import User from './user.entity';
+import { TypeOrmModule, getEntityManagerToken } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { EntityManager } from 'typeorm';
+ import User from './user.entity';
+
 import { UsersService } from './users.service'; 
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
-
     @Inject(UsersService)
 	private readonly service: UsersService;
 
-	@Get("/user/:username")
+    @Get()
+	async getUsers(): Promise<User[]> {
+        return await this.service.getAllUsers();
+    }
+
+    @Get(":id")
+	async getUserById(
+		@Param('id') id: number
+	): Promise<User> {
+		return await this.service.getUserById(id);
+	}
+
+	@Get("/search/:username")
 	async searchUser(
 		@Param('username') username: string
 	): Promise<User> {
--- api-rest/src/users/users.service.ts
+++ api-rest/src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, DataSource } from 'typeorm';
 import User from './user.entity';
 
 @Injectable()
@@ -10,17 +10,29 @@ export class UsersService {
 		public userRepository: Repository<User>,
 	) {}
     
+    getAllUsers(): Promise<User[]> {
+        return this.userRepository.find() // SELECT * from user
+    }
+
+    async getUserById(id: number): Promise<User>
+    {
+        
+        const user = await this.userRepository.findOne({where: {id: id}});
+        return user;
+      
+    }
+    
+    async getUserByUsername(username: string): Promise<User>
+    {
+        const user = await this.userRepository.findOne({where: {username: username}});
+        return user; 
+    }
+
     async addUser(user: User): Promise<User> {
 		const addedUser = this.userRepository.create(user);
 		await this.userRepository.save(addedUser);
 		return addedUser;
 	}
 
-    async getUserByUsername(username: string): Promise<User>
-	{
-		return await this.userRepository.findOne({
-			where: {"username": username}
-		})
-	}
 
 }
