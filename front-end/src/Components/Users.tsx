1b9a608662ed0dec701080be585991f0ee768f4b
samir bouzidi <fouleck30@hotmail.fr>
2022-09-05 15:37:40 +0200

    front-end

--- front-end/src/Components/Users.tsx
+++ front-end/src/Components/Users.tsx
@@ -1,10 +1,31 @@
 import '../styles/Components/Users.css'
+import { useState, useEffect } from 'react';
 
 export default function Users() {
+
+    const [userlist, setUserlist] : any = useState([{}]);
+
+    useEffect(() => {
+        let url = "http://localhost:4000/users";
+        fetch(url)
+        .then (response => response.json())
+        .then (data => setUserlist(data))
+    }, []) 
+    console.log(userlist[0].username);
+    
     return (
         <div className='users-content'>
-            <h2>Joueurs:</h2>
-            <p>En ligne:</p>
+                <p>Hors Ligne</p>
+            <div className='online'>   
+                {
+                    userlist.map((user : any)=> (
+                        
+                            <img key={user.id} className='user-avatar' src={user.avatar_url}></img>
+                        
+                    )
+                    )
+                }
+            </div>  
         </div>
 
     );
