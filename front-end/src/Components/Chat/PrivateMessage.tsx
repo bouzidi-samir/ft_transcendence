a217de34b9d318c7b4fdd260d9a2c30673a0066b
sbouzidi <fouleck30@hotmail.fr>
2022-12-17 21:20:34 +0100

    bloc

--- front-end/src/Components/Chat/PrivateMessage.tsx
+++ front-end/src/Components/Chat/PrivateMessage.tsx
@@ -1,4 +1,5 @@
 import {useDispatch, useSelector} from 'react-redux';
+import { useState, useEffect } from 'react';
 
 export default function PrivateMessage(props: any) {
     const {hostname} = document.location;
@@ -6,12 +7,33 @@ export default function PrivateMessage(props: any) {
     const {interlocutor} = props;
     const User = useSelector((state: any) => state.User);
     let Roomlist = useSelector((state: any) => state.RoomList);
+    const [blockedList, setBlockedList] = useState([]);
+
+    useEffect(() => {
+        let url = `http://${hostname}:4000/users/blockedPeople/${interlocutor.username}`;
+        fetch(url, {headers: 
+            {'Authorization': `Bearer ${User.JWT_token}`,
+            'Content-Type': 'application/json',
+            'cors': 'true'
+          },})
+        .then(ret => ret.json()).then((ret) => { 
+            let list : any  = ret.map((e: any) => (User.username));
+            setBlockedList(list)
+        }
+        )
+    }
+    , [openConversation]
+    )
    
     function openConversation(e : any) {
         e.preventDefault();
         let check = Roomlist.filter((e: any) => 
                 e.tag === User.nickname + interlocutor.nickname || e.tag === interlocutor.nickname + User.nickname
             )
+        if (blockedList.length > 0){
+            alert("Cet utilisateur vous a bloqué")
+            return;
+        }
        if (check.length > 0){
            dispatch({type: "RoomActive/setRoomActive",payload: check[0]});
             return;
