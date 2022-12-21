a4cc57e6ed0c786d02c815bf24576b8df23e2ae0
samir bouzidi <fouleck30@hotmail.fr>
2022-12-21 22:12:19 +0100

    reset

--- api-rest/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable, Inject } from '@nestjs/common';
-import { UsersService } from './users/users.service';
-
-@Injectable()
-export class AppService {
-  @Inject(UsersService)
-	public readonly users: UsersService;
-}
--- api-rest/src/auth/dto/TwoFactorAuthenticationCodeDto.ts
+++
@@ -1,7 +0,0 @@
-import { ArrayMinSize, IsArray, IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
-
-export class TwoFactorAuthenticationCodeDto {
-  @IsString()
-  @IsNotEmpty()
-  twoFactorAuthenticationCode: string;
-}
