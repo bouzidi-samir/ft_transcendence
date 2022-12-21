be7ea2cfd91cf202e2ee05f7d9bb2f61f1272203
samir bouzidi <fouleck30@hotmail.fr>
2022-12-21 12:38:51 +0100

    PrivateMessage.tsx

--- front-end/src/Components/Chat/PrivateMessage.tsx
+++ front-end/src/Components/Chat/PrivateMessage.tsx
@@ -1,6 +1,7 @@
 import {useDispatch, useSelector} from 'react-redux';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { set } from 'immer/dist/internal';
+import { SocketContext } from '../../Context/socket';
 
 export default function PrivateMessage(props: any) {
     const {hostname} = document.location;
@@ -10,6 +11,9 @@ export default function PrivateMessage(props: any) {
     const User = useSelector((state: any) => state.User);
     let Roomlist = useSelector((state: any) => state.RoomList);
     const [blockedList, setBlockedList] = useState([]);
+    const [update, setUpdate] = useState(0);
+    const socket = useContext(SocketContext);
+    const alertRoom = "NEW ROOM AVAILABLE !!!";
 
     useEffect(() => {
         let url = `http://${hostname}:4000/users/blockedPeople/${interlocutor.username}`;
@@ -24,32 +28,30 @@ export default function PrivateMessage(props: any) {
         }
         )
     }
-    , []
+    , [update]
     )
     
     async function updateBlockedList(){
         let url = `http://${hostname}:4000/users/blockedPeople/${interlocutor.username}`;
-        fetch(url, {headers: 
+        let rep = await fetch(url, {headers: 
             {'Authorization': `Bearer ${User.JWT_token}`,
             'Content-Type': 'application/json',
             'cors': 'true'
           },})
-        .then(ret => ret.json()).then((ret) => {
-            //let list : any  = ret.map((e: any) => (User.username));
-            setBlockedList(ret)
-        }
-        )
+        let list = await rep.json(); 
+        setBlockedList(list);
+        return (list);
     }
 
 
    
     async function openConversation(e : any) {
         e.preventDefault();
-        await updateBlockedList();
+        let list = await updateBlockedList();
         let check = Roomlist.filter((e: any) => 
                 e.tag === User.nickname + interlocutor.nickname || e.tag === interlocutor.nickname + User.nickname
             )
-        if (blockedList.some((e : any) => e.toUsername == User.username)){
+        if (list.some((e : any) => e.toUsername == User.username)){
             alert("Cet utilisateur vous a bloqué")
             return;
         }
@@ -66,7 +68,6 @@ export default function PrivateMessage(props: any) {
             privateMessage: true,
             password: ""
         }
-        dispatch({type: "Roomlist/addRoom", payload: newRoom,});
         let url = `http://${hostname}:4000/chat/createRoom`
         let response = fetch(url, {method : 'POST',
         headers: {
@@ -75,6 +76,8 @@ export default function PrivateMessage(props: any) {
         },
         body: JSON.stringify(newRoom)
     }).then(ret => ret.json())
+        dispatch({type: "Roomlist/addRoom", payload: newRoom,});
+        socket?.emit("newRoomClient", alertRoom);
     }
 
     return (
